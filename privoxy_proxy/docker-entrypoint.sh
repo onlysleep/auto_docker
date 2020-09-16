1e8b89438e7413330e94a363363611afaac9ca7a
chnxyzz <28676240+chnxyzz@users.noreply.github.com>
2020-09-16 16:59:43 +0800

    new

--- privoxy_proxy/docker-entrypoint.sh
+++ privoxy_proxy/docker-entrypoint.sh
@@ -5,12 +5,13 @@ if [ ! -n "$address" ]; then
   echo "need PROXY_ADDRESS_YOURSELF";
   exit 1;
 else
+    sed -i "s/listen-address  127.0.0.1:8118/listen-address  0.0.0.0:8118/g" /etc/privoxy/config;
     echo "forward-socks5t / $address ." >> /etc/privoxy/config;
 fi
 if [ ! -n "$allowip" ]; then
-  sed -i "s/listen-address  127.0.0.1:8118/listen-address  0.0.0.0:8118/g" /etc/privoxy/config;
+  echo "allow all ip"
 else
-    sed -i "s/listen-address  127.0.0.1:8118/listen-address  $allowip:8118/g" /etc/privoxy/config;
+  echo "permit-access $address " >> /etc/privoxy/config;
 fi   
 set -e
 privoxy --no-daemon /etc/privoxy/config ;
