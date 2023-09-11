c4cd9dee16d3c706fbd19a0573dd69c24caa354b
chnxyzz <28676240+chnxyzz@users.noreply.github.com>
2023-09-11 17:01:35 +0800

    Update init_conda.sh

--- personal_ubuntu/init_conda.sh
+++ personal_ubuntu/init_conda.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-for name in py27 py36 py37 py38 py39 py310
+for name in py27 py37 py38 py39 py310
 do
     /root/miniconda/bin/conda create -y -n $name python=2.7 && \
     /root/miniconda/bin/conda install -y -n $name requests numpy pandas requests gmpy2
 done
 
-for name in py36 py37 py38
+for name in py37 py38
 do 
     /root/miniconda/bin/conda run -n $name python -m pip install pwntools jupyter notebook libnum
 done
