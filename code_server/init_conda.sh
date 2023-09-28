231b008fba7915c27a24fcd32fbc2d79e03e802b
pwnxyzz_m1air <m1mac@dopwn.cn>
2023-09-28 23:12:08 +0800

    new

---
+++ code_server/init_conda.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+for version in 3.6 3.7 3.8 3.9 3.10 3.11
+do
+    name='py'${version/./}
+    echo $name
+    /root/miniconda/bin/conda create -y -n $name python=$version && \
+    /root/miniconda/bin/conda install -y -n $name requests numpy pandas requests gmpy2 libnum pwntools jupyter notebook
+done
+
+/root/miniconda/bin/conda create -y -n py27 python=2.7 && \
+/root/miniconda/bin/conda run -n py27 python -m pip install requests numpy pandas requests gmpy2 libnum pyelftools==0.29 pathlib2 pwntools
